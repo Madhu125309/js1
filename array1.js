16eebedc958d132334ff3de47047c130aade64f5
7997259422 <madhureddy85767@gmail.com>
2023-05-18 11:49:30 +0530

    hi

---
+++ array1.js
@@ -0,0 +1,7 @@
+let enames=["madhu", "sai", "dhanu", "yashwanth"]
+for(ename of enames)
+{
+    console.log(enames.length)
+    break;
+    
+}
--- for2.js
+++
@@ -1,4 +0,0 @@
-for(let i=1;i<=18;i=i+2)
-{
- console.log(i)
-}
