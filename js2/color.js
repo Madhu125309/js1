06806a5af5ae5a3863b436df42fffe547d85969f
7997259422 <madhureddy85767@gmail.com>
2023-05-25 17:09:05 +0530

    dom

---
+++ js2/color.js
@@ -0,0 +1,23 @@
+function cc_mouseOver() {
+    document.getElementById('mover').style.backgroundColor = "yellow";
+}
+
+function cc_mouseout() {
+    document.getElementById('mout').style.backgroundColor = "green";
+}
+
+function cc_mouseonClick() {
+    document.getElementById('monclick').style.backgroundColor ="black";
+}
+
+function cc_mouseondblClick() {
+    document.getElementById('mondblclick').style.backgroundColor ="pink";
+}
+
+function cc_mouseonfocus() {
+    document.getElementById('monfocus').style.backgroundColor ="red";
+} 
+
+function cc_mouseonblur(){
+    document.getElementById('monblur').style.backgroundColor ="brown";
+}
